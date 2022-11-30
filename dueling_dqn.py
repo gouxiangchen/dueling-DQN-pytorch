d7507cac81fee570beec39f3fca129dc9b0fe86b
GabrielDeml <gabrielddeml@gmail.com>
2022-11-30 15:16:42 -0500

    arstarst

--- dueling_dqn.py
+++ dueling_dqn.py
@@ -7,41 +7,11 @@ import random
 from itertools import count
 import torch.nn.functional as F
 from tensorboardX import SummaryWriter
-
+from model import QNetwork
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-class QNetwork(nn.Module):
-    def __init__(self):
-        super(QNetwork, self).__init__()
-
-        self.fc1 = nn.Linear(4, 64)
-        self.relu = nn.ReLU()
-        self.fc_value = nn.Linear(64, 256)
-        self.fc_adv = nn.Linear(64, 256)
-
-        self.value = nn.Linear(256, 1)
-        self.adv = nn.Linear(256, 2)
-
-    def forward(self, state):
-        y = self.relu(self.fc1(state))
-        value = self.relu(self.fc_value(y))
-        adv = self.relu(self.fc_adv(y))
-
-        value = self.value(value)
-        adv = self.adv(adv)
-
-        advAverage = torch.mean(adv, dim=1, keepdim=True)
-        Q = value + adv - advAverage
-
-        return Q
-
-    def select_action(self, state):
-        with torch.no_grad():
-            Q = self.forward(state)
-            action_index = torch.argmax(Q, dim=1)
-        return action_index.item()
 
 
 class Memory(object):
---
+++ model.py
@@ -0,0 +1,37 @@
+import torch.nn as nn
+import torch
+
+class QNetwork(nn.Module):
+    def __init__(self):
+        super(QNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(4, 64)
+        self.relu = nn.ReLU()
+        self.fc_value = nn.Linear(64, 256)
+        self.fc_adv = nn.Linear(64, 256)
+
+        self.value = nn.Linear(256, 1)
+        self.adv = nn.Linear(256, 2)
+
+    def forward(self, state):
+        y = self.relu(self.fc1(state))
+        value = self.relu(self.fc_value(y))
+        adv = self.relu(self.fc_adv(y))
+
+        value = self.value(value)
+        adv = self.adv(adv)
+
+        advAverage = torch.mean(adv, dim=1, keepdim=True)
+        Q = value + adv - advAverage
+
+        return Q
+
+    def half_forward(self, state):
+        return self.relu(self.fc1(state))
+
+    def select_action(self, state):
+        with torch.no_grad():
+            Q = self.forward(state)
+            action_index = torch.argmax(Q, dim=1)
+        return action_index.item()
+
